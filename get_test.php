02a81c116f7234b9d4af18c118bd9296005dd3ed
Developer77-77 <73919317+Developer77-77@users.noreply.github.com>
2022-11-12 11:45:22 -0600

    Add files via upload

---
+++ get_test.php
@@ -0,0 +1,17 @@
+<?php
+ 
+$get_name=$_GET["name"];
+ 
+$get_location=$_GET["location"];
+ 
+if( $get_name )
+ 
+{
+ 
+   echo "Name entered is:". $get_name ."<br>";
+ 
+   echo "Location:" .$get_location;
+ 
+}
+ 
+?>
